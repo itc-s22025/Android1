d13ed94ffea19763982fad88c87c2ca7dd86b744
s22025 <s22025@std.it-college.ac.jp>
2023-07-27 14:40:33 +0900

    7.1 データをリスト状に並べて表示

--- IntentSample/app/src/main/java/jp/ac/it_college/std/s22025/intentsample/MainActivity.kt
+++ IntentSample/app/src/main/java/jp/ac/it_college/std/s22025/intentsample/MainActivity.kt
@@ -2,10 +2,30 @@ package jp.ac.it_college.std.s22025.intentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import jp.ac.it_college.std.s22025.intentsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lvMenu.apply {
+            // アダプタとデータをセット
+            adapter = MenuListAdapter(menuList)
+
+            // RecyclerViewの表示方法を直線的にする
+            LinearLayoutManager(this@MainActivity).let {
+                layoutManager = it
+                // 区切り線を並べる方向に合わせて表示する
+                addItemDecoration(DividerItemDecoration(this@MainActivity, it.orientation))
+            }
+        }
+
+
     }
 }
