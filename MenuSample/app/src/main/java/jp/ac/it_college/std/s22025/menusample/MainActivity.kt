d25c7213096254ab415d04ffe9b628fc4fb88f6c
s22025 <s22025@std.it-college.ac.jp>
2023-08-08 16:07:34 +0900

    8.1 七章のサンプルと同じ動き

--- MenuSample/app/src/main/java/jp/ac/it_college/std/s22025/menusample/MainActivity.kt
+++ MenuSample/app/src/main/java/jp/ac/it_college/std/s22025/menusample/MainActivity.kt
@@ -1,11 +1,36 @@
 package jp.ac.it_college.std.s22025.menusample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import jp.ac.it_college.std.s22025.menusample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initList(binding.lvMenu)
+    }
+
+    private fun initList(view: RecyclerView){
+        view.adapter = MenuListAdapter(teishokuList, ::order)
+        val layoutManager = LinearLayoutManager(this)
+        view.layoutManager = layoutManager
+        view.addItemDecoration(DividerItemDecoration(this, layoutManager.orientation))
+
+    }
+
+    private fun order(name: String, price: Int){
+        startActivity(Intent(this, MenuThanksActivity::class.java).apply {
+            putExtra("menuName", name)
+            putExtra("menuPrice", price)
+        })
     }
 }
