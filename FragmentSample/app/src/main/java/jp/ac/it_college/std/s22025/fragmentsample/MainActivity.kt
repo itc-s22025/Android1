2c32d48fa79837e4ebe63323689fe5dc16816273
s22025 <s22025@std.it-college.ac.jp>
2023-08-24 12:11:27 +0900

    9章おわり

--- FragmentSample/app/src/main/java/jp/ac/it_college/std/s22025/fragmentsample/MainActivity.kt
+++ FragmentSample/app/src/main/java/jp/ac/it_college/std/s22025/fragmentsample/MainActivity.kt
@@ -1,10 +1,9 @@
 package jp.ac.it_college.std.s22025.fragmentsample
-
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.commit
-import android.os.Bundle
-import android.util.Log
 import jp.ac.it_college.std.s22025.fragmentsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +14,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // イベント識別名、LifecycleOwner,イベントリスナ を渡して待ち受ける
-        supportFragmentManager
-            .setFragmentResultListener(REQUEST_SELECTED_MENU, this, ::onSelectedMenu)
+        // イベント識別名、LifecycleOwner,イベントリスナ を渡して
+        // Fragment からの応答を待ち受ける
+        supportFragmentManager.run {
+            setFragmentResultListener(
+                REQUEST_SELECTED_MENU, this@MainActivity, ::onSelectedMenu
+            )
+            setFragmentResultListener(
+                REQUEST_BACK_MENU, this@MainActivity, ::onBackMenu
+            )
+        }
+
     }
 
     private fun onSelectedMenu(requestKey: String, bundle: Bundle) {
@@ -26,16 +33,30 @@ class MainActivity : AppCompatActivity() {
         // MenuThanksFragment を表示させる。
         supportFragmentManager.commit {
             setReorderingAllowed(true)
-            addToBackStack("Only List")
-            replace(
-                R.id.fragmentMainContainer, MenuThanksFragment::class.java,
-                bundleOf(
-                    ARG_NAME to bundle.getString(RESULT_NAME, ""),
-                    ARG_PRICE to bundle.getInt(RESULT_PRICE, 0)
-                )
+            val args = bundleOf(
+                ARG_NAME to bundle.getString(RESULT_NAME, ""),
+                ARG_PRICE to bundle.getInt(RESULT_PRICE, 0)
             )
+            // fragmentMainContainer があればスマホ版
+            if (binding.fragmentMainContainer != null) {
+                addToBackStack("Only List")
+                replace(R.id.fragmentMainContainer, MenuThanksFragment::class.java, args)
+            } else {
+                // fragmentMainContainer がないのでタブレットの横向き版
+                replace(R.id.fragmentThanksContainer, MenuThanksFragment::class.java, args)
+            }
         }
     }
 
-
+    private fun onBackMenu(requestKey: String, bundle: Bundle) {
+        if (binding.fragmentMainContainer != null) {
+            supportFragmentManager.popBackStack()
+        } else {
+            supportFragmentManager.commit {
+                binding.fragmentThanksContainer?.let {
+                    remove(it.getFragment())
+                }
+            }
+        }
+    }
 }
--- FragmentSample/app/src/main/java/jp/ac/it_college/std/s22025/fragmentsample/MenuThanksFragment.kt
+++ FragmentSample/app/src/main/java/jp/ac/it_college/std/s22025/fragmentsample/MenuThanksFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import jp.ac.it_college.std.s22025.fragmentsample.databinding.FragmentMenuThanksBinding
 
 internal const val ARG_NAME = "menuName"
 internal const val ARG_PRICE = "menuPrice"
+/* Fragment Result API を通じて Activity へデータを返すためのイベント名 */
+internal const val REQUEST_BACK_MENU = "backMenu"
 
 /**
  * 注文完了を表示するためのフラグメント
@@ -16,10 +19,8 @@ internal const val ARG_PRICE = "menuPrice"
 class MenuThanksFragment : Fragment() {
     // Bindingクラスのインスタンスを入れておくプロパティ(Nullable)
     private var _binding: FragmentMenuThanksBinding? = null
-
     // Bindingを non-null 型としてシンプルに使うための工夫
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,31 +28,25 @@ class MenuThanksFragment : Fragment() {
         _binding = FragmentMenuThanksBinding.inflate(inflater, container, false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         // arguments プロパティ経由で外部からパラメータを受け取る
         val menuName = arguments?.getString(ARG_NAME) ?: ""
         val menuPrice = arguments?.getInt(ARG_PRICE) ?: 0
-
         // ビューにパラメータをセットする。
         binding.tvMenuName.text = menuName
         binding.tvMenuPrice.text = "%,d".format(menuPrice)
-
         // ボタンのイベントリスナを設定
         binding.btThxBack.setOnClickListener(::onBackButtonClick)
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
     /**
      * 戻るボタンが押されたときの処理。フラグメントの履歴を遡る処理。
      */
     private fun onBackButtonClick(v: View) {
-        parentFragmentManager.popBackStack()
+        parentFragmentManager.setFragmentResult(REQUEST_BACK_MENU, bundleOf())
     }
 }
